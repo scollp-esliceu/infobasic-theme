32bb6c2610de332895536cbde9300bf0d4bff1a0
scollp-esliceu <scollp@esliceu.com>
2026-01-22 17:31:24 +0100

    COMMIT DIJOUS

--- functions.php
+++ functions.php
@@ -17,6 +17,7 @@ function infobasic_scripts(){
 
     wp_enqueue_script( "bootstrap_js", get_theme_file_uri("inc/js/bootstrap.min.js"), array("jquery"),"4.5", true );
     wp_enqueue_style( "bootstrap_css", get_theme_file_uri("inc/css/bootstrap.min.css"),array(), "4.5","all" );
+    wp_enqueue_style( "style_css", get_theme_file_uri("style.css"),array(), "1,0", "all" );
 
 }
 add_action( 'wp_enqueue_scripts', 'infobasic_scripts' );
